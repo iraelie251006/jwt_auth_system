5fc8cab4f8e49c4489a5a6b4db055a95f85e13f8
Elie <niyubwayoiraelie5777@gmail.com>
2025-11-27 16:00:07 +0200

    Installed mongoose & implemented user model.

---
+++ models/user.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    username: {type: String, required: true, unique: true},
+    email: {type: String, unique: true, required: true},
+    password: {type: String, required: true}
+});
+
+const User = mongoose.model('User', userSchema);
