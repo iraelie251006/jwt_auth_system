4aaab362f54a02a8bbfb195c891b00dbd4b29bc7
Elie <niyubwayoiraelie5777@gmail.com>
2025-11-28 10:12:27 +0200

    Improved code formatting.

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,31 +1,33 @@
 import jwt from "jsonwebtoken";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 const JWT_SECRET = process.env.SECRET;
 
 export const auth = async (req, res, next) => {
-    const authHeader = req.headers.authorization || '';
+  const authHeader = req.headers.authorization || "";
 
-    const [scheme, token] = authHeader.split(' ');
+  const [scheme, token] = authHeader.split(" ");
 
-    if (scheme !== 'Bearer' || !token) {
-        return res.status(401).json({ message: 'Missing or invalid Authorization header' });
+  if (scheme !== "Bearer" || !token) {
+    return res
+      .status(401)
+      .json({ message: "Missing or invalid Authorization header" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    const user = { id: decoded.id, email: decoded.email };
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).json({ message: "Access token expired" });
     }
-
-    try {
-        const decoded = jwt.verify(token, JWT_SECRET);
-
-        const user = {id: decoded.id, email: decoded.email}
-
-        req.user = user
-
-        next();
-    } catch (error) {
-        if (error.name === 'TokenExpiredError') {
-      return res.status(401).json({ message: 'Access token expired' });
-    }
-        return res.status(401).json({ message: 'Invalid token' });
-    }
-}
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
